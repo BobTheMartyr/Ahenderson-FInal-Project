43230c9a5accd57622e3ba4b4a78d3c8072c6921
Andy Henderson <ashenderson4372@gmail.com>
2025-03-09 09:55:50 -0400

    Adds degree.h file

---
+++ Ahenderson FInal Project/degree.h
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+enum DegreeProgram {
+	SECURITY,
+	NETWORK,
+	SOFTWARE
+};
--- Ahenderson FInal Project/student.cpp
+++ Ahenderson FInal Project/student.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 //Accessors
 
 void Student::setStudentID(string ID) {
