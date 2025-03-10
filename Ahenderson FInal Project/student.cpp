bb9094c59cff8f5497cb8936715cb1ab73c8b388
Andy Henderson <ashenderson4372@gmail.com>
2025-03-10 18:35:40 -0400

    Worked on the functions a bit and connected student.h to student.cpp and main.cpp

--- Ahenderson FInal Project/student.cpp
+++ Ahenderson FInal Project/student.cpp
@@ -1,3 +1,4 @@
+#include "student.h"
 #include <iostream>
 using namespace std;
 //Accessors
@@ -18,11 +19,11 @@ void Student::setEmail(string email) {
 	this->emailAddress = email;
 }
 
-void Student::setStudentAge(int age) {
+void Student::setStudentAge(string age) {
 	this->studentAge = age;
 }
 
-//TODO daysincourse
+//TODO void Student::setDaysInCourse(){}
 
 void Student::setDegreeProgram(string degree) {
 	this->degreeProgram = degree;
@@ -35,8 +36,18 @@ string Student::getFirstName() { return this->firstName; }
 string Student::getLastName() { return this->lastName; }
 string Student::getEmail() { return this->Email; }
 int Student::getStudentAge() { return this->studentAge; }
-//TODO get daysincourse
+//TODO Student::getDaysInCourse(){}
 string Student::getDegreeProgram() { return degreeProgram; }
+
+//Print
+void print(){
+	cout << this->getStudentID() << endl;
+	cout << "First Name: " << this->getFirstName() << endl;
+	cout << "Last Name: " << this->getLastName() << endl;
+	cout << "Age: " << this->getStudentAge() << endl;
+	//TODO daysincourse cout << "Days in course: " << 
+	cout << "Degree Program: " << this.getDegreeProgram() << endl;
+}
 
 
 Student::Student(string studentID, string firstName, string lastName, string email, int studentAge, int daysInCourse[], string degreeProgram) {
--- Ahenderson FInal Project/student.h
+++ Ahenderson FInal Project/student.h
@@ -1,35 +1,40 @@
-#include <iosteam>
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <iostream>
 using namespace std;
 
 
-Class Student{
-	private: 
-		string studentID;
-		string firstName;
-		string lastName;
-		string emailAddress;
-		int studentAge;
-		string degreeProgram;
-		int daysInCourse[3];
+class Student {
+private:
+	string studentID;
+	string firstName;
+	string lastName;
+	string emailAddress;
+	int studentAge;
+	string degreeProgram;
+	int daysInCourse[3];
 
-		Student(string studentID, string firstName, string lastName, string email, int studentAge, int daysInCourse[], string degreeProgram);
-	
-	public:
-		string getStudentID();
-		string getFirstName();
-		string getLastName();
-		string getEmail();
-		int getStudentAge();
-		int getDaysInCourse();
-		string getDegreeProgram();
+public:
+	string getStudentID();
+	string getFirstName();
+	string getLastName();
+	string getEmail();
+	int getStudentAge();
+	int getDaysInCourse();
+	string getDegreeProgram();
 
-		void setStudentID();
-		void setFirstName();
-		void setLastName();
-		void setEmail();
-		void setStudentAge();
-		void setDaysInCourse();
-		void setDegreeProgram();
+	void setStudentID( string ID );
+	void setFirstName( string inputFirstName );
+	void setLastName( string inputLastName );
+	void setEmail( string inputEmail );
+	void setStudentAge( string inputAge );
+	void setDaysInCourse();
+	void setDegreeProgram( string inputProgram );
+	void print();
 
+	Student(string studentID, string firstName, string lastName, string email, int studentAge, int daysInCourse[], string degreeProgram);
 
-}
+};
+
+#endif // !STUDENT_H
