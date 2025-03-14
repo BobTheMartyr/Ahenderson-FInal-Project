b18a60cffee35ee8c5e52e384fbe02c18dbe264f
Andrew Henderson <102596227+BobTheMartyr@users.noreply.github.com>
2025-03-14 16:25:12 -0400

    modifies the getters for daysincourse and degreeprogram

--- Ahenderson FInal Project/student.cpp
+++ Ahenderson FInal Project/student.cpp
@@ -2,66 +2,65 @@
 #include <iostream>
 #include <string>
 using namespace std;
-//Accessors
 
+
+//Constructor
+Student::Student(string studentID, string firstName, string lastName, string email, int studentAge, int days1, int days2, int days3, string degreeProgram) {
+	setStudentID(studentID);
+	setFirstName(firstName);
+	setLastName(lastName);
+	setEmail(email);
+	setStudentAge(studentAge);
+	setDaysInCourse(days1, days2, days3);
+	setDegreeProgram(degreeProgram);
+
+}
+
+//Setters
 void Student::setStudentID(string ID) {
 	this->studentID = ID;
 }
-
 void Student::setFirstName(string firstName) {
 	this->firstName = firstName;
 }
-
 void Student::setLastName(string lastName) {
 	this->lastName = lastName;
 }
-
 void Student::setEmail(string email) {
 	this->emailAddress = email;
 }
-
-void Student::setStudentAge(string age) {
+void Student::setStudentAge(int age) {
 	this->studentAge = age;
 }
-
-void Student::setDaysInCourse(days1, days2, days3) {
+void Student::setDaysInCourse(int days1, int days2, int days3) {
 	this->daysInCourse[0] = days1;
 	this->daysInCourse[1] = days2;
 	this->daysInCourse[2] = days3;
 }
-
-void Student::setDegreeProgram(string degree) {
-	this->degreeProgram = degree;
+void Student::setDegreeProgram(string degreeProgram) {
+	if (degreeProgram == "SECURITY") { this->degreeProgram = DegreeProgram::SECURITY; };
+	if (degreeProgram == "NETWORK") { this->degreeProgram = DegreeProgram::NETWORK; };
+	if (degreeProgram == "SOFTWARE") { this->degreeProgram = DegreeProgram::SOFTWARE; };
 }
 
 //Getters
-
 string Student::getStudentID() { return this->studentID; }
 string Student::getFirstName() { return this->firstName; }
 string Student::getLastName() { return this->lastName; }
 string Student::getEmail() { return this->Email; }
 int Student::getStudentAge() { return this->studentAge; }
-int Student::getDaysInCourse(){ return this->daysInCourse }
-string Student::getDegreeProgram() { return degreeProgram; }
+array<int, 3> Student::getDaysInCourse() { return daysInCourse };
+DegreeProgram Student::getDegreeProgram() { return this->degreeProgram; } 
 
 //Print
 void print(){
-	cout << this->getStudentID() << endl;
-	cout << "First Name: " << this->getFirstName() << endl;
-	cout << "Last Name: " << this->getLastName() << endl;
-	cout << "Age: " << this->getStudentAge() << endl;
-	//TODO daysincourse cout << "Days in course: " << 
-	cout << "Degree Program: " << this.getDegreeProgram() << endl;
+	cout << this->getStudentID() << "\t"
+		 << "First Name: " << this->getFirstName() << "\t"
+		 << "Last Name: " << this->getLastName() << "\t"
+		 << "Age: " >> this->getStudentAge() << "\t"
+		 << "Days in course: {" << this->daysInCourse[0] ", " << this->daysInCourse[1] << ", " << this->daysInCourse[2] << "}" << "\t"
+		 << "Degree Program: " << (this->getDegreeProgram() == 0 ? "Security" : (this->getDegreeProgram() == 1 ? "Network" : "Software")) << endl;
+
 }
 
 
-Student::Student(string studentID, string firstName, string lastName, string email, int studentAge, int daysInCourse[], string degreeProgram) {
-	this->studentID = studentID;
-	this->firstName = firstName;
-	this->lastName = lastName;
-	this->emailAddress = email;
-	this->studentAge = studentAge;
-
-	this->degreeProgram = degreeProgram;
-	
-}
